9acec4046ea2a492029074c9a10c8fd1e3b0320f
Matteo Collina <hello@matteocollina.com>
2016-08-03 16:27:36 +0200

    Added assertion to validate the value out from the schema.

--- test.js
+++ test.js
@@ -1,17 +1,20 @@
 'use strict'
 
 const test = require('tap').test
+const validator = require('is-my-json-valid')
 const build = require('.')
 
 function buildTest (schema, toStringify) {
   test(`render a ${schema.title} as JSON`, (t) => {
-    t.plan(2)
+    t.plan(3)
 
+    const validate = validator(schema)
     const stringify = build(schema)
     const output = stringify(toStringify)
 
     t.deepEqual(JSON.parse(output), toStringify)
     t.equal(output, JSON.stringify(toStringify))
+    t.ok(validate(JSON.parse(output)), 'valid schema')
   })
 }
 
