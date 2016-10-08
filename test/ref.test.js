9700ebc50ddd14a56618b891916f2de4297aad37
delvedor <tommydelved@gmail.com>
2016-10-08 10:47:45 +0200

    Updated test

--- test/ref.test.js
+++ test/ref.test.js
@@ -48,22 +48,31 @@ test('ref internal - properties', (t) => {
 test('ref external - properties', (t) => {
   t.plan(2)
 
-  const schema = {
-    title: 'object with $ref',
-    definitions: {
-      def: {
-        type: 'object',
-        properties: {
-          str: {
-            type: 'string'
+  const externalSchema = {
+    first: require('./ref.json'),
+    second: {
+      definitions: {
+        num: {
+          type: 'object',
+          properties: {
+            int: {
+              type: 'integer'
+            }
           }
         }
       }
-    },
+    }
+  }
+
+  const schema = {
+    title: 'object with $ref',
     type: 'object',
     properties: {
       obj: {
-        $ref: __dirname + '/ref.json#/definitions/def' // eslint-disable-line
+        $ref: 'first#/definitions/def'
+      },
+      num: {
+        $ref: 'second#/definitions/num'
       }
     }
   }
@@ -71,10 +80,13 @@ test('ref external - properties', (t) => {
   const object = {
     obj: {
       str: 'test'
+    },
+    num: {
+      int: 42
     }
   }
 
-  const stringify = build(schema)
+  const stringify = build(schema, { schema: externalSchema })
   const output = stringify(object)
 
   try {
@@ -84,7 +96,7 @@ test('ref external - properties', (t) => {
     t.fail()
   }
 
-  t.equal('{"obj":{"str":"test"}}', output)
+  t.equal('{"obj":{"str":"test"},"num":{"int":42}}', output)
 })
 
 test('ref internal - patternProperties', (t) => {
@@ -218,4 +230,59 @@ test('ref internal - pattern-additional Properties', (t) => {
   }
 
   t.equal('{"reg":{"str":"test"},"obj":{"str":"test"}}', output)
+})
+
+test('ref external - pattern-additional Properties', (t) => {
+  t.plan(2)
+
+  const externalSchema = {
+    first: require('./ref.json'),
+    second: {
+      definitions: {
+        num: {
+          type: 'object',
+          properties: {
+            int: {
+              type: 'integer'
+            }
+          }
+        }
+      }
+    }
+  }
+
+  const schema = {
+    title: 'object with $ref',
+    type: 'object',
+    properties: {},
+    patternProperties: {
+      reg: {
+        $ref: 'first#/definitions/def'
+      }
+    },
+    additionalProperties: {
+      $ref: 'second#/definitions/num'
+    }
+  }
+
+  const object = {
+    reg: {
+      str: 'test'
+    },
+    obj: {
+      int: 42
+    }
+  }
+
+  const stringify = build(schema, { schema: externalSchema })
+  const output = stringify(object)
+
+  try {
+    JSON.parse(output)
+    t.pass()
+  } catch (e) {
+    t.fail()
+  }
+
+  t.equal('{"reg":{"str":"test"},"obj":{"int":42}}', output)
 })
