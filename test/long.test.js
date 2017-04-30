14c00d9f9419b076812b69b21c2a19c68ec809ed
rochdev <roch.devost@gmail.com>
2017-04-30 13:58:29 -0400

    Add tests for long inside objects/arrays

--- test/long.test.js
+++ test/long.test.js
@@ -20,3 +20,45 @@ test(`render a long as JSON`, (t) => {
   t.equal(output, '18446744073709551615')
   t.ok(validate(JSON.parse(output)), 'valid schema')
 })
+
+test(`render an object with long as JSON`, (t) => {
+  t.plan(2)
+
+  const schema = {
+    title: 'object with long',
+    type: 'object',
+    properties: {
+      id: {
+        type: 'integer'
+      }
+    }
+  }
+
+  const validate = validator(schema)
+  const stringify = build(schema)
+  const output = stringify({
+    id: Long.fromString('18446744073709551615', true)
+  })
+
+  t.equal(output, '{"id":18446744073709551615}')
+  t.ok(validate(JSON.parse(output)), 'valid schema')
+})
+
+test(`render aan array with long as JSON`, (t) => {
+  t.plan(2)
+
+  const schema = {
+    title: 'array with long',
+    type: 'array',
+    items: {
+      type: 'integer'
+    }
+  }
+
+  const validate = validator(schema)
+  const stringify = build(schema)
+  const output = stringify([Long.fromString('18446744073709551615', true)])
+
+  t.equal(output, '[18446744073709551615]')
+  t.ok(validate(JSON.parse(output)), 'valid schema')
+})
