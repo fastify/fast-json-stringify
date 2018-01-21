a7744cb984c051f9fe46bf8376deeb3fd171753e
Tommaso Allevi <tomallevi@gmail.com>
2018-01-21 17:49:13 +0100

    Add test for checking on top level

--- test/throw.test.js
+++ test/throw.test.js
@@ -3,7 +3,7 @@
 const test = require('tap').test
 const build = require('..')
 
-test('Cannot be coerced to integer', t => {
+test('Cannot be coerced to integer / 1', t => {
   const values = [ true, false, null, Infinity, -Infinity, NaN, {}, '', '55 foobar', [], [5] ]
   t.plan(values.length)
 
@@ -24,7 +24,25 @@ test('Cannot be coerced to integer', t => {
   })
 })
 
-test('Cannot be coerced to boolean', t => {
+test('Cannot be coerced to integer / 2', t => {
+  const values = [ true, false, null, Infinity, -Infinity, NaN, {}, '', '55 foobar', [], [5] ]
+  t.plan(values.length)
+
+  const serializer = build({
+    type: 'integer'
+  })
+
+  values.forEach(function (v) {
+    try {
+      serializer(v)
+      t.fail()
+    } catch (e) {
+      t.equal(e.message, 'Cannot coerce to number')
+    }
+  })
+})
+
+test('Cannot be coerced to boolean / 1', t => {
   const values = [ 0, 42, -55.5, null, Infinity, -Infinity, NaN, {}, '', '55 foobar', [], [5] ]
   t.plan(values.length)
 
@@ -38,6 +56,24 @@ test('Cannot be coerced to boolean', t => {
   values.forEach(function (v) {
     try {
       serializer({ my_int: v })
+      t.fail()
+    } catch (e) {
+      t.equal(e.message, 'Cannot coerce to boolean')
+    }
+  })
+})
+
+test('Cannot be coerced to boolean / 2', t => {
+  const values = [ 0, 42, -55.5, null, Infinity, -Infinity, NaN, {}, '', '55 foobar', [], [5] ]
+  t.plan(values.length)
+
+  const serializer = build({
+    type: 'boolean'
+  })
+
+  values.forEach(function (v) {
+    try {
+      serializer(v)
       t.fail()
     } catch (e) {
       t.equal(e.message, 'Cannot coerce to boolean')
