44171706601cc1609463375e2edafba326119bff
delvedor <tommydelved@gmail.com>
2018-06-01 23:27:38 +0200

    Updated test

--- test/basic.test.js
+++ test/basic.test.js
@@ -328,3 +328,19 @@ test('patternProperties - throw on unknown type', (t) => {
     t.ok(err)
   }
 })
+
+test('render a single quote as JSON', (t) => {
+  t.plan(2)
+
+  const schema = {
+    type: 'string'
+  }
+  const toStringify = '" double quote'
+
+  const validate = validator(schema)
+  const stringify = build(schema)
+  const output = stringify(toStringify)
+
+  t.equal(output, JSON.stringify(toStringify))
+  t.ok(validate(JSON.parse(output)), 'valid schema')
+})
