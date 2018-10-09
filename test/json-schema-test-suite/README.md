3f6b175da4afc9942347add310240212f2e4c670
starptech <deusdustin@gmail.com>
2018-10-09 22:26:08 +0200

    basic support

---
+++ test/json-schema-test-suite/README.md
@@ -0,0 +1,11 @@
+# JSON-Schema-Test-Suite
+
+You can find all test cases [here](https://github.com/json-schema-org/JSON-Schema-Test-Suite).
+it contains a set of JSON objects that implementors of JSON Schema validation libraries can use to test their validators.
+
+# How to add another test case?
+
+* Navigate to [JSON-Schema-Test-Suite](https://github.com/json-schema-org/JSON-Schema-Test-Suite/tree/master/tests)
+* Choose a draft `draft4`, `draft6` or `draft7`
+* Copy & paste the `test-case.json` to the project and add a test file
+* When your tests fail open an issue or create a pull-request
---
+++ test/json-schema-test-suite/util.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const build = require('../..')
+
+function runTests (t, testsuite, skippedTests) {
+  for (const scenario of testsuite) {
+    const stringify = build(scenario.schema)
+    for (const test of scenario.tests) {
+      if (skippedTests.indexOf(test.description) !== -1) {
+        t.comment('skip %s', test.description)
+        continue
+      }
+      t.test(test.description, (t) => {
+        t.plan(1)
+        try {
+          let output = stringify(test.data)
+          t.equal(output, JSON.stringify(test.data), 'compare payloads')
+        } catch (err) {
+          if (test.valid === false) {
+            t.pass('payload is invalid')
+          } else {
+            t.fail('payload should be valid: ' + err.message)
+          }
+        }
+      })
+    }
+  }
+}
+
+function counTests (ts, skippedTests) {
+  return ts.reduce((a, b) => a + b.tests.length, 0) - skippedTests.length
+}
+
+module.exports = { runTests, counTests }
