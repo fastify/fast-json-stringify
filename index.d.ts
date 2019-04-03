2e9836461a0e5df5f26d113682dc4598ae68fcee
Michael de Wit <mjwwit@users.noreply.github.com>
2019-04-03 20:37:55 +0200

    Add TypeScript type declaration file (fixes #148) (#156)
    * Add TypeScript type declaration file (fixes #148)
    
    * Improve types and add tests for types
    
    * Add types for options and nullable

---
+++ index.d.ts
@@ -0,0 +1,168 @@
+import { Options as AjvOptions } from "ajv"
+declare namespace build {
+  interface BaseSchema {
+    /**
+     * Schema title
+     */
+    title?: string;
+    /**
+     * Schema description
+     */
+    description?: string;
+    /**
+     * A comment to be added to the schema
+     */
+    $comment?: string;
+    /**
+     * Default value to be assigned when no value is given in the document
+     */
+    default?: any;
+    /**
+     * A list of example values that match this schema
+     */
+    examples?: any[];
+    /**
+     * Additional schema definition to reference from within the schema
+     */
+    definitions?: Record<string, Schema>
+    /**
+     * A set of schemas of which at least one must match
+     */
+    anyOf?: Partial<Schema>[];
+    /**
+     * A set of schemas which must all match
+     */
+    allOf?: Partial<Schema>[];
+    /**
+     * A conditional schema to check, controls schemas defined in `then` and `else`
+     */
+    if?: Partial<Schema>;
+    /**
+     * A schema to apply if the conditional schema from `if` passes
+     */
+    then?: Partial<Schema>;
+    /**
+     * A schema to apply if the conditional schema from `if` fails
+     */
+    else?: Partial<Schema>;
+    /**
+     * Open API 3.0 spec states that any value that can be null must be declared `nullable`
+     * @default false
+     */
+    nullable?: boolean;
+  }
+
+  export interface RefSchema {
+    /**
+     * A json-pointer to a schema to use as a reference
+     */
+    $ref: string;
+  }
+
+  export interface StringSchema extends BaseSchema {
+    type: "string";
+  }
+
+  export interface IntegerSchema extends BaseSchema {
+    type: "integer";
+  }
+
+  export interface NumberSchema extends BaseSchema {
+    type: "number";
+  }
+
+  export interface NullSchema extends BaseSchema {
+    type: "null";
+  }
+
+  export interface BooleanSchema extends BaseSchema {
+    type: "boolean";
+  }
+
+  export interface ArraySchema extends BaseSchema {
+    type: "array";
+    /**
+     * The schema for the items in the array
+     */
+    items: Schema | {}
+  }
+
+  export interface TupleSchema extends BaseSchema {
+    type: "array";
+    /**
+     * The schemas for the items in the tuple
+     */
+    items: Schema[];
+  }
+
+  type ObjectProperties = Record<string, Partial<Schema>> & {
+    anyOf?: ObjectProperties[];
+    allOf?: ObjectProperties[];
+    if?: ObjectProperties;
+    then?: ObjectProperties;
+    else?: ObjectProperties;
+  }
+
+  export interface ObjectSchema extends BaseSchema {
+    type: "object";
+    /**
+     * Describe the properties of the object
+     */
+    properties?: ObjectProperties;
+    /**
+     * The required properties of the object
+     */
+    required?: string[];
+    /**
+     * Describe properties that have keys following a given pattern
+     */
+    patternProperties?: ObjectProperties;
+    /**
+     * Specifies whether additional properties on the object are allowed, and optionally what schema they should
+     * adhere to
+     * @default false
+     */
+    additionalProperties?: Schema | boolean;
+  }
+
+  export type Schema =
+    | RefSchema
+    | StringSchema
+    | IntegerSchema
+    | NumberSchema
+    | NullSchema
+    | BooleanSchema
+    | ArraySchema
+    | TupleSchema
+    | ObjectSchema;
+
+  export interface Options {
+    /**
+     * Optionally add an external definition to reference from your schema
+     */
+    schema?: Record<string, Schema>
+    /**
+     * Uglify the generated serialization function to get a performance increase on Node.js versions lower than 8.3.0
+     */
+    uglify?: boolean
+    /**
+     * Configure Ajv, which is used to evaluate conditional schemas and combined (anyOf) schemas
+     */
+    ajv?: AjvOptions
+  }
+}
+
+/**
+ * Build a stringify function using a schema of the documents that should be stringified
+ * @param schema The schema used to stringify values
+ * @param options The options to use (optional)
+ */
+declare function build(schema: build.StringSchema, options?: build.Options): (doc: string) => string;
+declare function build(schema: build.IntegerSchema | build.NumberSchema, options?: build.Options): (doc: number) => string;
+declare function build(schema: build.NullSchema, options?: build.Options): (doc: null) => "null";
+declare function build(schema: build.BooleanSchema, options?: build.Options): (doc: boolean) => string;
+declare function build(schema: build.ArraySchema | build.TupleSchema, options?: build.Options): (doc: any[]) => string;
+declare function build(schema: build.ObjectSchema, options?: build.Options): (doc: object) => string;
+declare function build(schema: build.Schema, options?: build.Options): (doc: object | any[] | string | number | boolean | null) => string;
+
+export = build;
---
+++ test/types/test.ts
@@ -0,0 +1,147 @@
+import build, { Schema } from '../..'
+
+// Number schemas
+const schema1: Schema = {
+    type: 'number'
+}
+const schema2: Schema = {
+    type: 'integer'
+}
+
+build(schema1)(25)
+build(schema2)(-5)
+
+// String schema
+const schema3: Schema = {
+    type: 'string'
+}
+
+build(schema3)('foobar')
+
+// Boolean schema
+const schema4: Schema = {
+    type: 'boolean'
+}
+
+build(schema4)(true)
+
+// Null schema
+const schema5: Schema = {
+    type: 'null'
+}
+
+build(schema5)(null)
+
+// Array schemas
+const schema6: Schema = {
+    type: 'array',
+    items: { type: 'number' }
+}
+const schema7: Schema = {
+    type: 'array',
+    items: [{ type: 'string'}, {type: 'integer'}]
+}
+
+build(schema6)([25])
+build(schema7)(['hello', 42])
+
+// Object schemas
+const schema8: Schema = {
+    type: 'object'
+}
+const schema9: Schema = {
+    type: 'object',
+    properties: {
+        foo: { type: 'string' },
+        bar: { type: 'integer' }
+    },
+    required: ['foo'],
+    patternProperties: {
+      'baz*': { type: 'null' }
+    },
+    additionalProperties: {
+      type: 'boolean'
+    }
+}
+
+build(schema8)({})
+build(schema9)({ foo: 'bar' })
+
+// Reference schemas
+const schema10: Schema = {
+  title: 'Example Schema',
+  definitions: {
+    num: {
+      type: 'object',
+      properties: {
+        int: {
+          type: 'integer'
+        }
+      }
+    },
+    str: {
+      type: 'string'
+    },
+    def: {
+      type: 'null'
+    }
+  },
+  type: 'object',
+  properties: {
+    nickname: {
+      $ref: '#/definitions/str'
+    }
+  },
+  patternProperties: {
+    'num': {
+      $ref: '#/definitions/num'
+    }
+  },
+  additionalProperties: {
+    $ref: '#/definitions/def'
+  }
+}
+
+build(schema10)({ nickname: '', num: { int: 5 }, other: null })
+
+// Conditional/Combined schemas
+const schema11: Schema = {
+  title: 'Conditional/Combined Schema',
+  type: 'object',
+  properties: {
+    something: {
+      anyOf: [
+        { type: 'string' },
+        { type: 'boolean' }
+      ]
+    }
+  },
+  if: {
+    properties: {
+      something: { type: 'string' }
+    }
+  },
+  then: {
+    properties: {
+      somethingElse: { type: 'number' }
+    }
+  },
+  else: {
+    properties: {
+      somethingElse: { type: 'null' }
+    }
+  }
+}
+
+build(schema11)({ something: 'a string', somethingElse: 42 })
+
+// With options
+const schema12: Schema = {
+  title: 'Some Schema',
+  type: 'object',
+  properties: {
+    str: { $ref: '#/definitions/string' }
+  }
+}
+
+build(schema12, { schema: { string: { type: 'string' } }, uglify: true, ajv: { jsonPointers: true } })
