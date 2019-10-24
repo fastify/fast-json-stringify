3737b92d99475dec6161a6c814a18c5fc4c487b4
Emanuele Di Saverio <Emanuele.di.Saverio@frogdesign.com>
2019-10-24 11:27:18 +0200

    added failing test for fast-json-stringify/issues/189

--- index.js
+++ index.js
@@ -881,6 +881,10 @@ function nested (laterCode, name, key, schema, externalSchema, fullSchema, subKe
 
   subKey = subKey || ''
 
+  if (schema.$ref) {
+    schema = refFinder(schema.$ref, fullSchema, fullSchema)
+  }
+  
   if (schema.type === undefined) {
     var inferedType = inferTypeByKeyword(schema)
     if (inferedType) {
