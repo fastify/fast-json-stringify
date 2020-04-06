5342c29083a6fdefe3788949276dee9cdc3053b2
Matteo Collina <matteo.collina@gmail.com>
2020-04-06 21:40:50 +0200

    Update test/date.test.js
    Co-Authored-By: Manuel Spigolon <behemoth89@gmail.com>

--- test/date.test.js
+++ test/date.test.js
@@ -379,7 +379,7 @@ test('serializing null value', t => {
       } = serialize(createSchema(prop), input)
 
       t.equal(output, '{"updatedAt":null}')
-      t.ok(validate(JSON.parse(output)), 'an empty string is not a date format')
+      t.ok(validate(JSON.parse(output)), 'valid schema')
     })
 
     t.test('format::time', t => {
