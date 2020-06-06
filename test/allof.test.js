12efdb981889b75421a96f5ebb6f2c2d940c2c10
Joel Mukuthu <joelmukuthu@gmail.com>
2020-06-06 13:14:32 +0200

    Test external $refs in allOf

--- test/allof.test.js
+++ test/allof.test.js
@@ -322,3 +322,62 @@ test('object with multiple $refs in allOf', (t) => {
     t.fail()
   }
 })
+
+test('object with external $refs in allOf', (t) => {
+  t.plan(1)
+
+  const externalSchema = {
+    first: {
+      definitions: {
+        id1: {
+          type: 'object',
+          properties: {
+            id1: {
+              type: 'integer'
+            }
+          }
+        }
+      }
+    },
+    second: {
+      id2: {
+        $id: '#id2',
+        type: 'object',
+        properties: {
+          id2: {
+            type: 'integer'
+          }
+        }
+      }
+    }
+  }
+
+  const schema = {
+    title: 'object with $ref in allOf',
+    type: 'object',
+    allOf: [
+      {
+        $ref: 'first#/definitions/id1'
+      },
+      {
+        $ref: 'second#id2'
+      }
+    ]
+  }
+
+  try {
+    const stringify = build(schema, { schema: externalSchema })
+    try {
+      const value = stringify({
+        id1: 1,
+        id2: 2,
+        id3: 3 // extra prop shouldn't be in result
+      })
+      t.is(value, '{"id1":1,"id2":2}')
+    } catch (e) {
+      t.fail()
+    }
+  } catch (e) {
+    t.fail()
+  }
+})
