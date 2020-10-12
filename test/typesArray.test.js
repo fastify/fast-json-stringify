1c58cbadaa0eff31cf213a55f7543a253e0ddc72
alemagio <alessandro.magionami@gmail.com>
2020-10-12 15:51:20 +0200

    test: removed redundant test

--- test/typesArray.test.js
+++ test/typesArray.test.js
@@ -436,23 +436,3 @@ test('should default to [] when type is array and object is null', (t) => {
   const valueStr = stringify({ arr: null })
   t.is(valueStr, '{"arr":[]}')
 })
-
-test('should not throw an error with default set to []', (t) => {
-  t.plan(1)
-  const schema = {
-    type: 'object',
-    properties: {
-      arr: {
-        type: 'array',
-        items: {
-          type: 'number'
-        },
-        default: []
-      }
-    }
-  }
-
-  const stringify = build(schema)
-  const valueStr = stringify({})
-  t.is(valueStr, '{"arr":[]}')
-})
