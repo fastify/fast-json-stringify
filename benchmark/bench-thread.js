34d96d5dce4f82f457747930a5fa20b625b59382
ivan-tymoshenko <bugagashenkj@gmail.com>
2022-05-23 15:12:02 +0300

    Run benchmarks in separate threads

---
+++ benchmark/bench-thread.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const { workerData: benchmark, parentPort } = require('worker_threads')
+
+const Benchmark = require('benchmark')
+Benchmark.options.minSamples = 500
+
+const suite = Benchmark.Suite()
+
+const FJS = require('..')
+const stringify = FJS(benchmark.schema)
+
+suite
+  .add(benchmark.name, () => {
+    stringify(benchmark.input)
+  })
+  .on('cycle', (event) => {
+    parentPort.postMessage(String(event.target))
+  })
+  .on('complete', () => {})
+  .run()
