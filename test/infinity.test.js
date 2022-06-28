cc2d4817a6534d3ecbbdbbb68a77a110e8b98810
yaki3355 <44410275+yaki3355@users.noreply.github.com>
2022-06-28 11:10:22 +0300

    serializer.js - asNumber(), asInteger() (#478)

--- test/infinity.test.js
+++ test/infinity.test.js
@@ -4,7 +4,9 @@ const test = require('tap').test
 const build = require('..')
 
 test('Finite numbers', t => {
-  const values = [-5, 0, -0, 1.33, Math.E, Number.EPSILON, Number.MAX_SAFE_INTEGER, Number.MAX_VALUE,
+  const values = [-5, 0, -0, 1.33, 99, 100.0,
+    Math.E, Number.EPSILON,
+    Number.MAX_SAFE_INTEGER, Number.MAX_VALUE,
     Number.MIN_SAFE_INTEGER, Number.MIN_VALUE]
 
   t.plan(values.length)
@@ -18,6 +20,26 @@ test('Finite numbers', t => {
   values.forEach(v => t.equal(stringify(v), JSON.stringify(v)))
 })
 
+test('Infinite integers', t => {
+  const values = [Infinity, -Infinity]
+
+  t.plan(values.length)
+
+  const schema = {
+    type: 'integer'
+  }
+
+  const stringify = build(schema)
+
+  values.forEach(v => {
+    try {
+      stringify(v)
+    } catch (err) {
+      t.equal(err.message, `The value "${v}" cannot be converted to an integer.`)
+    }
+  })
+})
+
 test('Infinite numbers', t => {
   const values = [Infinity, -Infinity]
 
@@ -29,11 +51,5 @@ test('Infinite numbers', t => {
 
   const stringify = build(schema)
 
-  values.forEach(v => {
-    try {
-      stringify(v)
-    } catch (err) {
-      t.equal(err.message, `The value "${v}" cannot be converted to a number.`)
-    }
-  })
+  values.forEach(v => t.equal(stringify(v), JSON.stringify(v)))
 })
