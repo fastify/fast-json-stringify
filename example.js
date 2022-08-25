05c92cfa5ad57ee1edc4cebae563c921410f51e7
Daniele Fedeli <danieligno10@gmail.com>
2022-08-25 18:54:37 +0200

    Restored example file

--- example.js
+++ example.js
@@ -3,12 +3,79 @@ const stringify = fastJson({
   title: 'Example Schema',
   type: 'object',
   properties: {
-    noRequired: { const: 'hello world' }
+    firstName: {
+      type: 'string'
+    },
+    lastName: {
+      type: 'string'
+    },
+    age: {
+      description: 'Age in years',
+      type: 'integer'
+    },
+    now: {
+      type: 'string'
+    },
+    birthdate: {
+      type: ['string'],
+      format: 'date-time'
+    },
+    reg: {
+      type: 'string'
+    },
+    obj: {
+      type: 'object',
+      properties: {
+        bool: {
+          type: 'boolean'
+        }
+      }
+    },
+    arr: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          str: {
+            type: 'string'
+          }
+        }
+      }
+    }
   },
-  required: ['noRequired']
+  required: ['now'],
+  patternProperties: {
+    '.*foo$': {
+      type: 'string'
+    },
+    test: {
+      type: 'number'
+    },
+    date: {
+      type: 'string',
+      format: 'date-time'
+    }
+  },
+  additionalProperties: {
+    type: 'string'
+  }
 })
 
-const result = stringify({
-})
-
-console.log({ result })
+console.log(
+  stringify({
+    firstName: 'Matteo',
+    lastName: 'Collina',
+    age: 32,
+    now: new Date(),
+    reg: /"([^"]|\\")*"/,
+    foo: 'hello',
+    numfoo: 42,
+    test: 42,
+    strtest: '23',
+    arr: [{ str: 'stark' }, { str: 'lannister' }],
+    obj: { bool: true },
+    notmatch: 'valar morghulis',
+    notmatchobj: { a: true },
+    notmatchnum: 42
+  })
+)
