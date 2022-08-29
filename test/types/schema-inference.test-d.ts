9d5d765f71f111de082ed68599782216d1ee1323
Daniele Fedeli <danieligno10@gmail.com>
2022-08-29 13:15:47 +0200

    Indented test file with space

--- test/types/schema-inference.test-d.ts
+++ test/types/schema-inference.test-d.ts
@@ -3,13 +3,13 @@ import build from "../..";
 
 // Schema with constant value
 const schema = {
-	type: "object",
-	properties: {
-		foo: {
-			const: "bar",
-		},
-	},
-	additionalProperties: false,
+  type: "object",
+  properties: {
+    foo: {
+      const: "bar",
+    },
+  },
+  additionalProperties: false,
 } as const;
 const stringify = build(schema);
 
@@ -21,12 +21,12 @@ stringify({ foo: "bar" });
 
 // Schema with property multiple types
 const schema1 = {
-	type: "object",
-	properties: {
-		foo: {
-			type: ["string", "integer", "null"],
-		},
-	},
+  type: "object",
+  properties: {
+    foo: {
+      type: ["string", "integer", "null"],
+    },
+  },
 } as const;
 const stringify1 = build(schema1);
 expectError(stringify1({ foo: true }));
@@ -37,44 +37,44 @@ stringify1({ foo: null });
 
 // Schema with nested properties
 const schema2 = {
-	type: "object",
-	properties: {
-		foo: {
-			type: "object",
-			properties: {
-				bar: { type: "object", properties: { baz: { type: "string" } } },
-			},
-			required: ["bar"],
-		},
-	},
+  type: "object",
+  properties: {
+    foo: {
+      type: "object",
+      properties: {
+        bar: { type: "object", properties: { baz: { type: "string" } } },
+      },
+      required: ["bar"],
+    },
+  },
 } as const;
 const stringify2 = build(schema2);
 expectError(
-	stringify2({
-		foo: {
-			bar: { baz: 1 },
-		},
-	})
+  stringify2({
+    foo: {
+      bar: { baz: 1 },
+    },
+  })
 );
 expectError(
-	stringify2({
-		foo: {
-			bar: null,
-		},
-	})
+  stringify2({
+    foo: {
+      bar: null,
+    },
+  })
 );
 stringify2({ foo: { bar: { baz: "baz" } } });
 stringify2({ foo: { bar: {} } });
 
 // With inference
 interface Schema {
-	id: string;
-	a?: number;
+  id: string;
+  a?: number;
 }
 
 const stringify3 = build({
-	type: "object",
-	properties: { a: { type: "string" } },
+  type: "object",
+  properties: { a: { type: "string" } },
 });
 stringify3<Schema>({ id: "123" });
 stringify3<Schema>({ a: 123, id: "123" });
