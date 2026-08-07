e5e141e785d36c3d055c0dba39256009d987233e
Rob Day <rob.day@lindushealth.com>
2026-08-07 09:10:32 +0100

    Add option to precompile AJV schemas to avoid lazy-compilation hit on first request

--- index.js
+++ index.js
@@ -160,6 +160,7 @@ function build (schema, options) {
     refResolver: new RefResolver(),
     rootSchemaId: schema.$id || `__fjs_root_${schemaIdCounter++}`,
     validatorSchemasIds: new Set(),
+    validatorSchemaRefs: new Set(),
     mergedSchemasIds: new Map(),
     recursiveSchemas: new Set(),
     recursivePaths: new Set(),
@@ -265,6 +266,10 @@ function build (schema, options) {
     for (const [schemaId, schema] of Object.entries(dependencies)) {
       validator.addSchema(schema, schemaId)
     }
+  }
+
+  if (options.compileValidators) {
+    validator.compileSchemas(context.validatorSchemaRefs)
   }
 
   if (options.mode === 'debug') {
@@ -1196,6 +1201,7 @@ function buildOneOf (context, location, input) {
 
     const nestedResult = buildValue(context, mergedLocation, input)
     const schemaRef = getValidatorSchemaRef(context, optionLocation)
+    context.validatorSchemaRefs.add(schemaRef)
 
     code += `
       ${index === 0 ? 'if' : 'else if'}(validator.validate("${schemaRef}", ${input})) {
@@ -1229,6 +1235,7 @@ function buildIfThenElse (context, location, input) {
 
   const ifLocation = location.getPropertyLocation('if')
   const ifSchemaRef = getValidatorSchemaRef(context, ifLocation)
+  context.validatorSchemaRefs.add(ifSchemaRef)
 
   const thenLocation = location.getPropertyLocation('then')
   let thenMergedSchemaId = context.mergedSchemasIds.get(thenSchema)
